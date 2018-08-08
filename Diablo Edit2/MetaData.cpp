9cbcf355dde422c3ad3c28ab259958ccce49e279
DoZerg <DoZerg@DESKTOP-9OMNGEA>
2018-08-08 22:25:32 +0100

    Add min/max/def to property field;

--- Diablo Edit2/MetaData.cpp
+++ Diablo Edit2/MetaData.cpp
@@ -5,20 +5,43 @@
 
 using namespace std;
 
+//struct CPropertyField
+
+const CPropertyField & CPropertyField::Normalze() {
+	ASSERT(0 <= bits && 0 <= min);
+	if (0 == max)
+		max = (1 << bits) - 1;
+	ASSERT(min <= max);
+	return *this;
+}
+
 //class CPropertyMetaData
 
-CPropertyMetaData::CPropertyMetaData(vector<pair<int, int>> fields)
-	: fields_(fields) {
-	bitsSum_ = accumulate(fields_.begin(), fields_.end(), 0, [](auto s, auto f) { return s + f.first; });
+CPropertyMetaData::CPropertyMetaData(const vector<CPropertyField> & fields, int def)
+	: fields_(fields)
+	, def_(def)
+{
+	bitsSum_ = accumulate(fields_.begin(), fields_.end(), 0, [](auto s, auto f) { return s + f.bits; });
 }
 
 vector<int> CPropertyMetaData::Parse(DWORD value) const {
 	vector<int> ret;
 	for (auto & f : fields_) {
-		ret.push_back((value & ((DWORD(1) << f.first) - 1)) + f.second);
-		value >>= f.first;
+		ret.push_back((value & ((DWORD(1) << f.bits) - 1)) + f.base);
+		value >>= f.bits;
 	}
-	return ret;
+	return move(ret);
+}
+
+vector<tuple<int, int, int>> CPropertyMetaData::GetParams(DWORD value) const {
+	vector<tuple<int, int, int>> ret;
+	for (auto & f : fields_) {
+		if (f.bits < 1)
+			break;
+		ret.emplace_back(value & ((DWORD(1) << f.bits) - 1), f.min, f.max);
+		value >>= f.bits;
+	}
+	return move(ret);
 }
 
 //CSFormat
